36a179d1473875dc6f72c7962cc9310c39c13ae5
WYX199952 <3288733131@qq.com>
2023-07-11 10:24:46 +0800

    Site updated: 2023-07-11 10:24:41

--- js/matery.js
+++ js/matery.js
@@ -187,3 +187,24 @@ if (localStorage.getItem('isDark') === '1') {
     document.body.classList.remove('DarkMode');
     $('#sum-moon-icon').removeClass("fa-sun").addClass('fa-moon')
 }
+
+// 网站主题切换模式开始
+        if (localStorage.getItem('themespring') === '1') {
+            document.body.classList.add('themespring');
+            }
+            else if (matchMedia('(prefers-color-scheme: themespring)').matches) {
+            document.body.classList.add('themespring');
+        }
+
+        function switchThemes() {
+            var body = document.body;
+            if(body.classList.contains('themespring')){
+              document.body.classList.remove('themespring');
+              localStorage.setItem('themespring','0');
+              return;
+            } else {
+              document.body.classList.add('themespring');
+              localStorage.setItem('themespring','1');
+              return;
+            }
+          };
